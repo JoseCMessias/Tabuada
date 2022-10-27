e0760ae3a6b38460f6fd5136bc08579b8149f9bc
Jose Messias <Josecmessias22@gmail.com>
2022-10-27 10:08:41 -0300

    Tabuada

---
+++ Tabuada/Program.cs
@@ -0,0 +1,48 @@
+﻿
+class Tabuada
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("=============== Tabuada ===============");
+        Console.Write("Digite a operação (+ | - | * | /): ");
+        char n = char.Parse(Console.ReadLine());
+        Console.Write("Digite um valor para a operação: ");
+
+        if (n == '+')
+        {        
+            var n1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                int soma = i + n1;
+                Console.WriteLine($"{i} + {n1} = {soma}");
+            }
+        }
+        if (n == '-')
+        {
+            var n1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                int Menos = i - n1;
+                Console.WriteLine($"{i} - {n1} = {Menos}");
+            }
+        }
+        if (n == '*')
+        {
+            var n1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                int Mult = i * n1;
+                Console.WriteLine($"{i} * {n1} = {Mult}");
+            }
+        }
+        if (n == '/')
+        {
+            var n1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                int Div = i / n1;
+                Console.WriteLine($"{i} / {n1} = {Div}");
+            }
+        }
+    }
+}
